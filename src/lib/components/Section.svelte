7105f7d530e82f8155431b929d56e5d1c4809dad
Laura <ladeezn11@gmail.com>
2024-06-06 19:09:02 +0100

    section component

---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let theme = "normal"
+
+  const themeMap = {
+    normal: {
+      heading: "var(--dark-sage)",
+      text: "var(--blue-sand)",
+      background: "var(--warm-sand)",
+    },
+    medium: {
+      heading: "var(--dark-crayola)",
+      text: "var(--dark-crayola)",
+      background: "var(--crayola)",
+    },
+    dark: {
+      heading: "var(--warm-sand)",
+      text: "var(--warm-sand)",
+      background: "var(--dark-crayola)",
+    },
+  }
+</script>
+
+<section style="background: {themeMap[theme].background}; color: {themeMap[theme].text};">
+  <!-- magic portal whatever you write in the Section tag is places in the portal tag -->
+  <slot></slot>
+</section>
+
+<style>
+  section {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+</style>
