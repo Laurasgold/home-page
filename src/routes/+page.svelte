abe4e35a4a1082f67fb1c4090fdc77be80fb7703
Laura <ladeezn11@gmail.com>
2024-06-18 20:28:13 +0100

    made the portfolio cards responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,7 @@
 <Section theme="medium" id="social">
   <div class="social">
     <h2>Follow my social media</h2>
-    <div class="socialLinks">
+    <div class="social-links">
       <Instagram />
       <Linkedin />
       <Github />
@@ -26,55 +26,27 @@
 <Section id="mission">Mission</Section>
 
 <Section theme="dark" id="portfolio">
-  <h2 class="portfolio">Portfolio</h2>
-  <section class="portfolioContainer">
-    <div class="portfolioBackground">
-      <div class="sectionDescription">blah blah</div>
+  <h2 class="portfolio-title">Portfolio</h2>
+  <div class="portfolio-container">
+    <div class="portfolio-card">
+      <p>blah blah</p>
     </div>
 
-    <div class="portfolioBackground">
-      <div class="sectionDescription">blah blah</div>
+    <div class="portfolio-card">
+      <p>blah blah</p>
     </div>
 
-    <div class="portfolioBackground">
-      <div class="sectionDescription">blah blah</div>
+    <div class="portfolio-card">
+      <p>blah blah</p>
     </div>
-  </section>
+  </div>
 </Section>
-<Section id="contactme">Contact me</Section>
+<Section id="contact-me">Contact me</Section>
 
 <style>
   h1 {
     font-size: var(--font-size-hero);
     font-family: var(--title-font);
-  }
-  .portfolioBackground {
-    background: var(--cadet);
-    height: 600px;
-    width: 420px;
-    margin: 25px;
-    border-radius: 5%;
-  }
-
-  .portfolioContainer {
-    display: flex;
-    justify-content: space-evenly;
-    padding-bottom: 50px;
-    margin: 1px;
-  }
-  .portfolio {
-    display: flex;
-    justify-content: center;
-    padding: 236px;
-    font-family: var(--title-font);
-    font-size: var(--font-size-large);
-    font-weight: 400;
-  }
-
-  .sectionDescription {
-    display: flex;
-    justify-content: center;
-    padding-top: 8px;
   }
 
   .social h2 {
@@ -84,20 +56,53 @@
     padding-top: clamp(120px, 1.5vw, 330px);
   }
   /* need to work on padding top and bottom depending on when the other parts of the site ar added */
-  .socialLinks {
+  .social-links {
     display: flex;
     justify-content: space-around;
     gap: 2rem;
-    padding-top: 7%;
+    padding-top: 3rem;
     padding-bottom: clamp(120px, 1vw, 330px);
     margin: 0 auto;
     max-width: 1028px;
   }
 
-  @media screen and (max-width: 800px) {
-    .portfolioContainer {
+  :global(.social-links svg) {
+    height: var(--font-size-medium);
+    width: var(--font-size-medium);
+  }
+
+  .portfolio-card {
+    background: var(--cadet);
+    height: 600px;
+    max-width: 400px;
+    border-radius: var(--card-radius);
+  }
+
+  .portfolio-container {
+    display: grid;
+    grid-template-columns: repeat(3, minmax(200px, 420px));
+    justify-content: space-around;
+    margin: 0 auto;
+    padding: 3rem 0;
+  }
+
+  .portfolio-title {
+    display: flex;
+    justify-content: center;
+    font-size: var(--font-size-large);
+    padding-top: 3rem;
+  }
+
+  @media screen and (max-width: 1306px) {
+    .portfolio-container {
+      display: flex;
       flex-direction: column;
-      justify-content: center;
+      align-items: center;
+    }
+    .portfolio-card {
+      height: clamp(14.063rem, 20.613vw + 9.425rem, 26.25rem);
+      width: clamp(18.75rem, 12.685vw + 15.896rem, 26.25rem);
+      margin: 2rem auto;
     }
   }
 </style>
