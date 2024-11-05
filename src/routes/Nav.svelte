63487c5c8f14735604c58b84581410fd69d958b0
Laura <ladeezn11@gmail.com>
2024-11-05 14:45:09 +0000

    hero, and clamps updated

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -20,7 +20,7 @@
     border-top: 2px var(--dark-sage) solid;
     border-bottom: 2px var(--dark-sage) solid;
     padding: 2rem 1rem;
-    font-size: var(--font-size-small);
+    font-size: var(--font-size-xsmall);
     font-family: var(--title-font);
   }
 </style>
